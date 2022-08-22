cbf17c2187ac1c4680e2c43eabaa16e5208f4d49
Avinash Kumar <105039208+avinash-7star@users.noreply.github.com>
2022-08-22 23:53:22 +0530

    Add files via upload

---
+++ .github/workflows/flyods traingle.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long int
+// #define rep(i,a,b) for(ll i=s;i<b;i++)
+
+int main()
+{
+    ll t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cout<<"Case #"<<i<<": ";
+        double area=1,lar,rar,tar;
+        ll r,a,b;
+        cin>>r>>a>>b;
+        while(a>0 && b>0)
+        {
+          double pi = 2 * acos(0.0);
+      cout<<pi<<" ";
+          /*
+            rar = pi*r*r;
+            a*= r;
+            lar = pi*a*a;
+            b = a/b;
+            rar = pi*b*b;
+            tar= lar + rar;
+            if(b==0 || a==0)
+            {
+                break;                
+            }
+        }
+        cout<<tar<<endl;
+    */
+    }
+    
+    return 0;
+}
+
+
+
+
